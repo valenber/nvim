d2ad57b94fb9892ebfe06b605508d7a02bc852ce
Valentin Berlin <valentin.berlin@qonto.com>
2026-03-30 21:32:59 +0200

    add oxfmt formatter config

--- lua/user/plugins/formatting.lua
+++ lua/user/plugins/formatting.lua
@@ -6,21 +6,28 @@ return {
 
     conform.setup({
       formatters_by_ft = {
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        javascriptreact = { "prettier" },
-        typescriptreact = { "prettier" },
-        handlebars = { "prettier" },
+        javascript = { "oxfmt", "prettier", stop_after_first = true },
+        typescript = { "oxfmt", "prettier", stop_after_first = true },
+        javascriptreact = { "oxfmt", "prettier", stop_after_first = true },
+        typescriptreact = { "oxfmt", "prettier", stop_after_first = true },
+        handlebars = { "oxfmt", "prettier", stop_after_first = true },
+        json = { "oxfmt", "prettier", stop_after_first = true },
         css = { "prettier" },
         scss = { "prettier" },
         html = { "prettier" },
-        json = { "prettier" },
         yaml = { "prettier" },
         markdown = { "prettier" },
         graphql = { "prettier" },
         lua = { "stylua" },
       },
       formatters = {
+        oxfmt = {
+          command = "oxfmt",
+          args = { "--stdin-filepath", "$FILENAME" },
+          stdin = true,
+          cwd = require("conform.util").root_file({ ".oxfmtrc.json" }),
+          require_cwd = true,
+        },
         prettier = {
           -- Prettier 3.6.2 has a bug where --config-path doesn't work with stdin
           -- Use tmpfile mode instead so prettier can discover config properly
